416ef7b2f2f09602561ce8809a993ef1e895d9b1
Vitalik Buterin <v@buterin.com>
2014-04-06 13:31:55 -0400

    pybtctool fixes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='bitcoin',
-      version='1.1.7',
+      version='1.1.8',
       description='Python Bitcoin Tools',
       author='Vitalik Buterin',
       author_email='vbuterin@gmail.com',
