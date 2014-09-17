fe6249f491778be8ea860221cf12f1d071c83652
Nikolaos Korasidis <renelvon@gmail.com>
2014-09-17 17:41:23 +0300

    Whitespace fixes per PEP8 (except E501).

--- test.py
+++ test.py
@@ -12,35 +12,37 @@ if argv[1] == 'y':
     print "Starting ECC arithmetic tests"
 for i in range(8 if argv[1] == 'y' else 0):
     print "### Round %d" % (i+1)
-    x,y = random.randrange(2**256), random.randrange(2**256)
-    print multiply(multiply(G,x),y)[0] == multiply(multiply(G,y),x)[0]
-    print add_pubkeys(multiply(G,x),multiply(G,y))[0] == multiply(G,add_privkeys(x,y))[0]
-    hx, hy = encode(x%N,16,64), encode(y%N,16,64)
-    print multiply(multiply(G,hx),hy)[0] == multiply(multiply(G,hy),hx)[0]
-    print add_pubkeys(multiply(G,hx),multiply(G,hy))[0] == multiply(G,add_privkeys(hx,hy))[0]
+    x, y = random.randrange(2**256), random.randrange(2**256)
+    print multiply(multiply(G, x), y)[0] == multiply(multiply(G, y), x)[0]
+    print add_pubkeys(multiply(G, x), multiply(G, y))[0] == multiply(G, add_privkeys(x, y))[0]
+    hx, hy = encode(x % N, 16, 64), encode(y % N, 16, 64)
+    print multiply(multiply(G, hx), hy)[0] == multiply(multiply(G, hy), hx)[0]
+    print add_pubkeys(multiply(G, hx), multiply(G, hy))[0] == multiply(G, add_privkeys(hx, hy))[0]
     h1601 = b58check_to_hex(pubtoaddr(privtopub(x)))
-    h1602 = b58check_to_hex(pubtoaddr(multiply(G,hx),23))
+    h1602 = b58check_to_hex(pubtoaddr(multiply(G, hx), 23))
     print h1601 == h1602
     p = privtopub(sha256(str(x)))
-    if i%2 == 1: p = changebase(p,16,256)
+    if i % 2 == 1:
+        p = changebase(p, 16, 256)
     print decompress(compress(p)) == p
-    print multiply(divide(G,x),x)[0] == G[0]
+    print multiply(divide(G, x), x)[0] == G[0]
 
 if argv[2] == 'y':
     print "Starting Electrum wallet internal consistency tests"
     for i in range(3):
         seed = sha256(str(random.randrange(2**40)))[:32]
         mpk = electrum_mpk(seed)
-        print 'seed: ',seed
-        print 'mpk: ',mpk
+        print 'seed: ', seed
+        print 'mpk: ', mpk
         for i in range(5):
-            pk = electrum_privkey(seed,i)
-            pub = electrum_pubkey((mpk,seed)[i%2],i)
+            pk = electrum_privkey(seed, i)
+            pub = electrum_pubkey((mpk, seed)[i % 2], i)
             pub2 = privtopub(pk)
-            print 'priv: ',pk
-            print 'pub: ',pub
+            print 'priv: ', pk
+            print 'pub: ', pub
             print pub == pub2
-            if pub != pub2: print 'DOES NOT MATCH!!!!\npub2: '+pub2
+            if pub != pub2:
+                print 'DOES NOT MATCH!!!!\npub2: '+pub2
 
 if argv[3] == 'y':
     # Requires Electrum
@@ -50,23 +52,24 @@ if argv[3] == 'y':
     seed = str(json.loads(os.popen("electrum -w %s getseed" % wallet).read())['seed'])
     addies = json.loads(os.popen("electrum -w %s listaddresses" % wallet).read())
     for i in range(5):
-        if addies[i] != electrum_address(seed,i,0):
-            print "Address does not match!!!, seed: %s, i: %d" % (seed,i)
+        if addies[i] != electrum_address(seed, i, 0):
+            print "Address does not match!!!, seed: %s, i: %d" % (seed, i)
 
     print "Electrum-style signing and verification tests, against actual Electrum"
     for i in range(8):
         alphabet = "1234567890qwertyuiopasdfghjklzxcvbnm"
-        msg = ''.join([random.choice(alphabet) for i in range(random.randrange(20,200))])
+        msg = ''.join([random.choice(alphabet) for i in range(random.randrange(20, 200))])
         addy = random.choice(addies)
-        wif = os.popen('electrum -w %s dumpprivkey %s' % (wallet, addy)).readlines()[-2].replace('"','').strip()
+        wif = os.popen('electrum -w %s dumpprivkey %s' % (wallet, addy)).readlines()[-2].replace('"', '').strip()
         priv = b58check_to_hex(wif)
         pub = privtopub(priv)
-    
+
         sig = os.popen('electrum -w %s signmessage %s %s' % (wallet, addy, msg)).readlines()[-1].strip()
-        verified = ecdsa_verify(msg,sig,pub)
+        verified = ecdsa_verify(msg, sig, pub)
         print "Verified" if verified else "Verification error"
-        rec = ecdsa_recover(msg,sig)
-        if pub == rec: print "Recovery successful"
+        rec = ecdsa_recover(msg, sig)
+        if pub == rec:
+            print "Recovery successful"
         if pub != rec or not verified:
             print "msg: "+msg
             print "sig: "+sig
@@ -76,22 +79,22 @@ if argv[3] == 'y':
             print "Recovery error"
             print "original  pub: "+pub, hex_to_point(pub)[1]
             print "recovered pub: "+rec
-    
-        mysig = ecdsa_sign(msg,priv)
-        v = os.popen('electrum -w %s verifymessage %s %s %s' % (wallet,addy, sig, msg)).read()
+
+        mysig = ecdsa_sign(msg, priv)
+        v = os.popen('electrum -w %s verifymessage %s %s %s' % (wallet, addy, sig, msg)).read()
         print v
 
 if argv[4] == 'y':
     print "Transaction-style signing and verification tests"
     for i in range(10):
         alphabet = "1234567890qwertyuiopasdfghjklzxcvbnm"
-        msg = ''.join([random.choice(alphabet) for i in range(random.randrange(20,200))])
+        msg = ''.join([random.choice(alphabet) for i in range(random.randrange(20, 200))])
         priv = sha256(str(random.randrange(2**256)))
         pub = privtopub(priv)
-        sig = ecdsa_tx_sign(msg,priv)
-        v = ecdsa_tx_verify(msg,sig,pub)
+        sig = ecdsa_tx_sign(msg, priv)
+        v = ecdsa_tx_verify(msg, sig, pub)
         print "Verified" if v else "Verification error"
-        rec = ecdsa_tx_recover(msg,sig)
+        rec = ecdsa_tx_recover(msg, sig)
         print "Recovered" if pub in rec else "Recovery failed"
 
 if argv[5] == 'y':
@@ -106,16 +109,16 @@ if argv[7] == 'y':
     privs = [sha256(str(random.randrange(2**256))) for x in range(4)]
     pubs = [privtopub(priv) for priv in privs]
     addresses = [pubtoaddr(pub) for pub in pubs]
-    mscript = mk_multisig_script(pubs[1:],2,3)
+    mscript = mk_multisig_script(pubs[1:], 2, 3)
     msigaddr = p2sh_scriptaddr(mscript)
-    tx = mktx(['01'*32+':1','23'*32+':2'],[msigaddr+':20202',addresses[0]+':40404'])
-    tx1 = sign(tx,1,privs[0])
-    sig1 = multisign(tx,0,mscript,privs[1])
-    print "Verifying sig1:",verify_tx_input(tx1,0,mscript,sig1,pubs[1])
-    sig3 = multisign(tx,0,mscript,privs[3])
-    print "Verifying sig3:",verify_tx_input(tx1,0,mscript,sig3,pubs[3])
-    tx2 = apply_multisignatures(tx1,0,mscript,[sig1,sig3])
-    print "Outputting transaction: ",tx2
+    tx = mktx(['01'*32+':1', '23'*32+':2'], [msigaddr+':20202', addresses[0]+':40404'])
+    tx1 = sign(tx, 1, privs[0])
+    sig1 = multisign(tx, 0, mscript, privs[1])
+    print "Verifying sig1:", verify_tx_input(tx1, 0, mscript, sig1, pubs[1])
+    sig3 = multisign(tx, 0, mscript, privs[3])
+    print "Verifying sig3:", verify_tx_input(tx1, 0, mscript, sig3, pubs[3])
+    tx2 = apply_multisignatures(tx1, 0, mscript, [sig1, sig3])
+    print "Outputting transaction: ", tx2
 
 if argv[8] == 'y':
     # Created with python-ecdsa 0.9
@@ -123,12 +126,12 @@ if argv[8] == 'y':
     # class gen:
     #     def order(self): return 115792089237316195423570985008687907852837564279074904382605163141518161494337
     # dummy = gen()
-    # for i in range(10): ecdsa.rfc6979.generate_k(dummy,i,hashlib.sha256,hashlib.sha256(str(i)).digest())
+    # for i in range(10): ecdsa.rfc6979.generate_k(dummy, i, hashlib.sha256, hashlib.sha256(str(i)).digest())
     test_vectors = [32783320859482229023646250050688645858316445811207841524283044428614360139869L, 109592113955144883013243055602231029997040992035200230706187150761552110229971L, 65765393578006003630736298397268097590176526363988568884298609868706232621488L, 85563144787585457107933685459469453513056530050186673491900346620874099325918L, 99829559501561741463404068005537785834525504175465914981205926165214632019533L, 7755945018790142325513649272940177083855222863968691658328003977498047013576L, 81516639518483202269820502976089105897400159721845694286620077204726637043798L, 52824159213002398817852821148973968315579759063230697131029801896913602807019L, 44033460667645047622273556650595158811264350043302911918907282441675680538675L, 32396602643737403620316035551493791485834117358805817054817536312402837398361L]
     print "Beginning RFC6979 deterministic signing tests"
     for i in range(10):
-        ti = test_vectors[i] 
-        mine = deterministic_generate_k(bin_sha256(str(i)),encode(i,256,32))
+        ti = test_vectors[i]
+        mine = deterministic_generate_k(bin_sha256(str(i)), encode(i, 256, 32))
         if ti == mine:
             print "Test vector matches"
         else:
@@ -138,36 +141,40 @@ if argv[8] == 'y':
 
 if argv[9] == 'y':
     # From https://en.bitcoin.it/wiki/BIP_0032
-    def full_derive(key,chain):
-        if len(chain) == 0: return key
-        elif chain[0] == 'pub': return full_derive(bip32_privtopub(key),chain[1:])
-        else: return full_derive(bip32_ckd(key,chain[0]),chain[1:])
+    def full_derive(key, chain):
+        if len(chain) == 0:
+            return key
+        elif chain[0] == 'pub':
+            return full_derive(bip32_privtopub(key), chain[1:])
+        else:
+            return full_derive(bip32_ckd(key, chain[0]), chain[1:])
     test_vectors = [
-        [ [], 'xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi' ],
-        [ ['pub'], 'xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8' ],
-        [ [ 2**31 ], 'xprv9uHRZZhk6KAJC1avXpDAp4MDc3sQKNxDiPvvkX8Br5ngLNv1TxvUxt4cV1rGL5hj6KCesnDYUhd7oWgT11eZG7XnxHrnYeSvkzY7d2bhkJ7' ],
-        [ [ 2**31, 1 ], 'xprv9wTYmMFdV23N2TdNG573QoEsfRrWKQgWeibmLntzniatZvR9BmLnvSxqu53Kw1UmYPxLgboyZQaXwTCg8MSY3H2EU4pWcQDnRnrVA1xe8fs' ],
-        [ [ 2**31, 1, 2**31 + 2], 'xprv9z4pot5VBttmtdRTWfWQmoH1taj2axGVzFqSb8C9xaxKymcFzXBDptWmT7FwuEzG3ryjH4ktypQSAewRiNMjANTtpgP4mLTj34bhnZX7UiM' ],
-        [ [ 2**31, 1, 2**31 + 2, 'pub', 2, 1000000000], 'xpub6H1LXWLaKsWFhvm6RVpEL9P4KfRZSW7abD2ttkWP3SSQvnyA8FSVqNTEcYFgJS2UaFcxupHiYkro49S8yGasTvXEYBVPamhGW6cFJodrTHy' ]
+        [[], 'xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi'],
+        [['pub'], 'xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8'],
+        [[2**31], 'xprv9uHRZZhk6KAJC1avXpDAp4MDc3sQKNxDiPvvkX8Br5ngLNv1TxvUxt4cV1rGL5hj6KCesnDYUhd7oWgT11eZG7XnxHrnYeSvkzY7d2bhkJ7'],
+        [[2**31, 1], 'xprv9wTYmMFdV23N2TdNG573QoEsfRrWKQgWeibmLntzniatZvR9BmLnvSxqu53Kw1UmYPxLgboyZQaXwTCg8MSY3H2EU4pWcQDnRnrVA1xe8fs'],
+        [[2**31, 1, 2**31 + 2], 'xprv9z4pot5VBttmtdRTWfWQmoH1taj2axGVzFqSb8C9xaxKymcFzXBDptWmT7FwuEzG3ryjH4ktypQSAewRiNMjANTtpgP4mLTj34bhnZX7UiM'],
+        [[2**31, 1, 2**31 + 2, 'pub', 2, 1000000000], 'xpub6H1LXWLaKsWFhvm6RVpEL9P4KfRZSW7abD2ttkWP3SSQvnyA8FSVqNTEcYFgJS2UaFcxupHiYkro49S8yGasTvXEYBVPamhGW6cFJodrTHy']
     ]
     print "Beginning BIP0032 tests"
     mk = bip32_master_key('000102030405060708090a0b0c0d0e0f'.decode('hex'))
     print 'Master key:', mk
     for tv in test_vectors:
-        left, right = full_derive(mk,tv[0]), tv[1]
-        if left == right: print "Test vector matches"
+        left, right = full_derive(mk, tv[0]), tv[1]
+        if left == right:
+            print "Test vector matches"
         else:
             print "Test vector does not match"
             print tv[0]
-            print [x.encode('hex') if isinstance(x,str) else x for x in bip32_deserialize(left)]
-            print [x.encode('hex') if isinstance(x,str) else x for x in bip32_deserialize(right)]
+            print [x.encode('hex') if isinstance(x, str) else x for x in bip32_deserialize(left)]
+            print [x.encode('hex') if isinstance(x, str) else x for x in bip32_deserialize(right)]
 
 if argv[10] == 'y':
     print "Starting address and script generation consistency tests"
     for i in range(5):
         a = privtoaddr(random_key())
         print a == script_to_address(address_to_script(a))
-        b = privtoaddr(random_key(),5)
+        b = privtoaddr(random_key(), 5)
         print b == script_to_address(address_to_script(b))
 
 if argv[11] == 'y':
@@ -206,13 +213,13 @@ if argv[11] == 'y':
     except AssertionError:
         print 'ripemd160 test failed.'
         success = False
-    
+
     if success:
         print "ripemd160 test successful."
 
 if argv[12] == 'y':
     print 'Testing script vs address outputs...'
-    
+
     addr0 = '1Lqgj1ThNfwLgHMp5qJUerYsuUEm8vHmVG'
     script0 = '76a914d99f84267d1f90f3e870a5e9d2399918140be61d88ac'
     addr1 = '31oSGBBNrpCiENH3XMZpiP6GTC4tad4bMy'
@@ -222,15 +229,15 @@ if argv[12] == 'y':
                'value': 158000}]
 
     outputs = [None] * 8
-    outputs[0] = [{'address' : addr0, 'value' : 1000}, {'address' : addr1, 'value' : 2000}]
-    outputs[1] = [{'script' : script0, 'value' : 1000}, {'address' : addr1, 'value' : 2000}]
-    outputs[2] = [{'address' : addr0, 'value' : 1000}, {'script' : script1, 'value' : 2000}]
-    outputs[3] = [{'script' : script0, 'value' : 1000}, {'script' : script1, 'value' : 2000}]
+    outputs[0] = [{'address': addr0, 'value': 1000}, {'address': addr1, 'value': 2000}]
+    outputs[1] = [{'script': script0, 'value': 1000}, {'address': addr1, 'value': 2000}]
+    outputs[2] = [{'address': addr0, 'value': 1000}, {'script': script1, 'value': 2000}]
+    outputs[3] = [{'script': script0, 'value': 1000}, {'script': script1, 'value': 2000}]
     outputs[4] = [addr0 + ':1000', addr1 + ':2000']
     outputs[5] = [script0 + ':1000', addr1 + ':2000']
     outputs[6] = [addr0 + ':1000', script1 + ':2000']
     outputs[7] = [script0 + ':1000', script1 + ':2000']
-    
+
     for i in range(len(outputs)):
         outs = outputs[i]
         tx_struct = deserialize(mktx(inputs, outs))
