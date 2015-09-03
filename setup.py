1c4f8b0b0e920d4d90a02ad5a7569d4ef6bd6642
vub <Vitalik Buterin>
2015-09-03 10:14:41 -0400

    DER encoding fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='bitcoin',
-      version='1.1.35',
+      version='1.1.36',
       description='Python Bitcoin Tools',
       author='Vitalik Buterin',
       author_email='vbuterin@gmail.com',
