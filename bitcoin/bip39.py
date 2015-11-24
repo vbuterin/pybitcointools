cad251bd0b07803a9acebc9eb9f06c5361006ace
Steve132 <steve@soapforge.com>
2015-11-24 10:59:44 -0500

    Adding bip39

--- bitcoin/bip39.py
+++ bitcoin/bip39.py
@@ -1,10 +1,22 @@
 import hashlib
 import os.path
+import binascii
+import random
 from bisect import bisect_left
 
-wordlist_english=list(open(os.path.join(os.path.dirpath(os.path.realpath(__file__)),'english.txt'),'r'))
-def _eint_to_bytes(entint,entbits):
-	return binascii.unhexlify(hex(entint)[2:].zfill(entbits//4).rstrip("L"))
+wordlist_english=list(open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'english.txt'),'r'))
+
+
+
+
+def eint_to_bytes(entint,entbits):
+	a=hex(entint)[2:].rstrip('L').zfill(32)
+	print(a)
+	return binascii.unhexlify(a)
+
+def mnemonic_int_to_words(mint,mint_num_words,wordlist=wordlist_english):
+	backwords=[wordlist[(mint >> (11*x)) & 0x7FF].strip() for x in range(mint_num_words)]	
+	return backwords[::-1]
 	
 def entropy_cs(entbytes):
 	entropy_size=8*len(entbytes)
@@ -12,17 +24,13 @@ def entropy_cs(entbytes):
 	hd=hashlib.sha256(entbytes).hexdigest()
 	csint=int(hd,16) >> (256-checksum_size)
 	return csint,checksum_size
-
-def mnemonic_int_to_words(mint,mint_num_words,wordlist=wordlist_english):
-	backwords=[wordlist[(mint >> (11*x)) & 0x7FF].strip() for x in range(mint_num_words)]	
-	return backwords[::-1]
 	
 def entropy_to_words(entbytes,wordlist=wordlist_english):
-	if(len(entbytes) < 4 || len(entbytes) % 4 != 0):
+	if(len(entbytes) < 4 or len(entbytes) % 4 != 0):
 		raise ValueError("The size of the entropy must be a multiple of 4 bytes (multiple of 32 bits)")
 	entropy_size=8*len(entbytes)
 	csint,checksum_size = entropy_cs(entbytes)
-
+	entint=int(binascii.hexlify(entbytes),16)
 	mint=(entint << checksum_size) | csint
 	mint_num_words=(entropy_size+checksum_size)//11
 	
@@ -73,11 +81,39 @@ def mnemonic_to_seed(mnemonic_phrase,passphrase=""):
 	try:
 		from hashlib import pbkdf2_hmac
 		def pbkdf2_hmac_sha256(password,salt,iters=2048):
-			return pbkdf2_hmac(name='sha512',password=password,salt=salt,iters)
+			return pbkdf2_hmac(name='sha512',password=password,salt=salt,iters=iters)
 	except:
 		from Crypto.Protocol.KDF import PBKDF2
 		from Crypto.Hash import SHA512,HMAC
 		
 		def pbkdf2_hmac_sha256(password,salt,iters=2048):
-			return PBKDF2(password=password,salt=salt,dkLen=64,count=iters,prf:lambda p,s: HMAC.new(p,s,SHA512).digest())
+			return PBKDF2(password=password,salt=salt,dkLen=64,count=iters,prf=lambda p,s: HMAC.new(p,s,SHA512).digest())
 	return pbkdf2_hmac_sha256(password=mnemonic_phrase,salt="mnemonic"+passphrase)
+
+def words_mine(prefix,entbits,satisfunction,wordlist=wordlist_english,randombits=random.getrandbits):
+	prefix_bits=len(prefix)*11
+	mine_bits=entbits-prefix_bits
+	pint=words_to_mnemonic_int(prefix,wordlist)
+	pint<<=mine_bits
+	dint=randombits(mine_bits)
+	count=0
+	while(not satisfunction(entropy_to_words(eint_to_bytes(pint+dint,entbits)))):
+		dint=randombits(mine_bits)
+		if((count & 0xFFFF) == 0):
+			print("Searched %f percent of the space" % (float(count)/float(1 << mine_bits)))
+
+	return entropy_to_words(eint_to_bytes(pint+dint,entbits))
+
+if __name__=="__main__":
+	import json
+	testvectors=json.load(open('vectors.json','r'))
+	for v in testvectors['english']:
+		ebytes=binascii.unhexlify(v[0])
+		w=' '.join(entropy_to_words(ebytes))
+		seed=mnemonic_to_seed(w)
+		print(w==v[1])
+				
+		print(v[2])
+		print(binascii.hexlify(seed))
+		
+
