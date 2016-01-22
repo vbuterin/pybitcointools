59c7976ec793cebe7984b74ff356c61a634a578d
Steve132 <steve@soapforge.com>
2016-01-22 17:22:34 -0500

    Fixed mining algorithm

--- bitcoin/mnemonic.py
+++ bitcoin/mnemonic.py
@@ -3,60 +3,71 @@ import os.path
 import binascii
 from os import urandom as secure_random_bytes
 from bisect import bisect_left
+import re
 
-wordlist_english=[w.strip() for w in open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'english.txt'),'r')]
+wordlist_english = [w.strip() for w in open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'english.txt'), 'r')]
 
-def eint_to_bytes(entint,entbits):
-	a=hex(entint)[2:].rstrip('L').zfill(entbits//4)
-	return binascii.unhexlify(a)
+def eint_to_bytes(entint, entbits):
+    a = hex(entint)[2:].rstrip('L').zfill(entbits // 4)
+    return binascii.unhexlify(a)
 
-def mnemonic_int_to_words(mint,mint_num_words,wordlist=wordlist_english):
-	backwords=[wordlist[(mint >> (11*x)) & 0x7FF].strip() for x in range(mint_num_words)]	
+
+def mnemonic_int_to_words(mint, mint_num_words, wordlist=wordlist_english):
+	backwords = [wordlist[(mint >> (11 * x)) & 0x7FF].strip() for x in range(mint_num_words)]
 	return backwords[::-1]
-	
+
+
 def entropy_cs(entbytes):
-	entropy_size=8*len(entbytes)
-	checksum_size=entropy_size//32
-	hd=hashlib.sha256(entbytes).hexdigest()
-	csint=int(hd,16) >> (256-checksum_size)
-	return csint,checksum_size
-	
-def entropy_to_words(entbytes,wordlist=wordlist_english):
-	if(len(entbytes) < 4 or len(entbytes) % 4 != 0):
-		raise ValueError("The size of the entropy must be a multiple of 4 bytes (multiple of 32 bits)")
-	entropy_size=8*len(entbytes)
-	csint,checksum_size = entropy_cs(entbytes)
-	entint=int(binascii.hexlify(entbytes),16)
-	mint=(entint << checksum_size) | csint
-	mint_num_words=(entropy_size+checksum_size)//11
-	
-	return mnemonic_int_to_words(mint,mint_num_words,wordlist)
+	entropy_size = 8 * len(entbytes)
+	checksum_size = entropy_size // 32
+	hd = hashlib.sha256(entbytes).hexdigest()
+	csint = int(hd, 16) >> (256 - checksum_size)
+	return csint, checksum_size
 
-def words_split(wordstr,wordlist=wordlist_english):
-	words=wordstr.split()	
-	for w in words:
-		if(w not in wordlist):
-			raise Exception("Word %s not in wordlist" % (w))
-	return words
 
-def words_to_mnemonic_int(words,wordlist=wordlist_english):
-	if(isinstance(words,str)):
-		words=words_split(words,wordlist)
-	return sum([wordlist.index(w) << (11*x) for x,w in enumerate(words[::-1])])
+def entropy_to_words(entbytes, wordlist=wordlist_english):
+    if(len(entbytes) < 4 or len(entbytes) % 4 != 0):
+        raise ValueError("The size of the entropy must be a multiple of 4 bytes (multiple of 32 bits)")
+    entropy_size = 8 * len(entbytes)
+    csint, checksum_size = entropy_cs(entbytes)
+    entint = int(binascii.hexlify(entbytes), 16)
+    mint = (entint << checksum_size) | csint
+    mint_num_words = (entropy_size + checksum_size) // 11
 
-def words_verify(words,wordlist=wordlist_english):
-	if(isinstance(words,str)):
-		words=words_split(words,wordlist)
-	
-	mint = words_to_mnemonic_int(words,wordlist)
-	mint_bits=len(words)*11
-	cs_bits=mint_bits//32
-	entropy_bits=mint_bits-cs_bits
-	eint=mint >> cs_bits
-	csint=mint & ((1 << cs_bits)-1)
-	ebytes=eint_to_bytes(eint,entropy_bits)
-	ecsint,ecsint_size=entropy_cs(ebytes)
+    return mnemonic_int_to_words(mint, mint_num_words, wordlist)
+
+
+def words_split(wordstr, wordlist=wordlist_english):
+    words = wordstr.split()
+    for w in words:
+        if(w not in wordlist):
+            raise Exception("Word %s not in wordlist" % (w))
+    return words
+
+
+def words_to_mnemonic_int(words, wordlist=wordlist_english):
+	if(isinstance(words, str)):
+		words = words_split(words, wordlist)
+	return sum([wordlist.index(w) << (11 * x) for x, w in enumerate(words[::-1])])
+
+
+def _mint_verify(mint, mint_bits):
+	cs_bits = mint_bits // 32
+	entropy_bits = mint_bits - cs_bits
+	eint = mint >> cs_bits
+	csint = mint & ((1 << cs_bits) - 1)
+	ebytes = eint_to_bytes(eint, entropy_bits)
+	ecsint, ecsint_size = entropy_cs(ebytes)
 	return csint == ecsint
+
+
+def words_verify(words, wordlist=wordlist_english):
+    if(isinstance(words, str)):
+        words = words_split(words, wordlist)
+
+    mint=words_to_mnemonic_int(words, wordlist)
+    mint_bits=len(words)*11
+    return _mint_verify(mint,mint_bits)
 
 def mnemonic_to_seed(mnemonic_phrase,passphrase=u''):
 	try:
@@ -93,19 +104,65 @@ def words_generate(num_bits_entropy=128,num_words=None,randombits=_getrandbits,w
     rint=randombits(num_bits_entropy)
     return entropy_to_words(eint_to_bytes(rint,num_bits_entropy),wordlist)
     
-def words_mine(prefix,entbits,satisfunction,wordlist=wordlist_english,randombits=_getrandbits):
-	prefix_bits=len(prefix)*11
-	mine_bits=entbits-prefix_bits
-	pint=words_to_mnemonic_int(prefix,wordlist)
-	pint<<=mine_bits
-	dint=randombits(mine_bits)
-	count=0
-	while(not satisfunction(entropy_to_words(eint_to_bytes(pint+dint,entbits)))):
-		dint=randombits(mine_bits)
-		if((count & 0xFFFF) == 0):
-			print("Searched %f percent of the space" % (float(count)/float(1 << mine_bits)))
+    
+#pattern syntax: a word is (\w*)(?::(\w*))?...but split it first
+def words_mine(pattern,valid=lambda x : True ,wordlist=wordlist_english,randombits=_getrandbits):
+    if(isinstance(pattern,str)):
+        pattern=pattern.split()
+    if(len(pattern) % 3 != 0):
+        raise RuntimeError("The number of words in the pattern must be a multiple of 3")
+    
+    rec=re.compile(r'(\w*)(?::(\w*))?')
+    ranges=[]
 
-	return entropy_to_words(eint_to_bytes(pint+dint,entbits))
+    for word in pattern:
+        m=rec.match(word)
+        if(not m):
+            raise RuntimeError("Could not parse pattern word %s" % (word))
+        lower=0
+        upper=1 << 11
+        lw,uw=m.group(1,2)
+        if(lw):
+            if(lw != ""):
+                try:
+                    lower=int(lw) & 0x7FF
+                except:
+                    lower=next((index for index,value in enumerate(wordlist) if lw <= value))
+        if(uw):
+            if(uw != ""):
+                try:
+                    upper=int(uw) & 0x7FF
+                except:
+                    upper=next((index for index,value in enumerate(wordlist) if uw <= value))
+        elif(lw):
+            upper=next((index for index,value in enumerate(wordlist[::-1]) if lw >= value[:len(lw)]))
+            upper=2048-upper
+        ranges.append((lower,upper))
+                    
+    total_randomness=reduce(lambda a,b: a*(b[1]-b[0]),ranges,1)
+    
+    mint_bits=len(pattern)*11
+    def gen_mint(rangess,randombitss):
+        mint=0
+        for r in rangess:
+            mint<<=11
+            mint|=r[0] + randombitss(11) % (r[1]-r[0])
+        return mint
+        
+    tint=gen_mint(ranges,randombits)
+    count=0
+ 
+    while(not _mint_verify(tint,mint_bits) or not valid(mnemonic_int_to_words(tint,len(pattern),wordlist))):
+        tint=gen_mint(ranges,randombits)
+        
+        count+=1
+        if((count & 0xFFFF) == 0):
+            print("Searched %f percent of the space" % (100.0*float(count)/float(total_randomness)))
+
+    return mnemonic_int_to_words(tint,len(pattern),wordlist)
+    
+        
+
 
 if __name__=="__main__":
 	import json
